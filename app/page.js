68363a758e01940d2c76e1820d78455fddce968f
OneManAndAToolbox <onemanandatoolboxuk@gmail.com>
2026-02-19 19:37:34 +0000

    Fix text alignment for Ottoman Bed project description

--- app/page.js
+++ app/page.js
@@ -666,7 +666,7 @@ export default function Home() {
                                     </div>
                                 </div>
                                 <div className="job-panel" data-job="general-job-4">
-                                    <div className="gallery-text"><p>Disassembled, transported, and reassembled in a tight space.</p></div>
+                                    <div className="gallery-text"><p style={{ textAlign: 'left' }}>Disassembled, transported, and reassembled in a tight space.</p></div>
                                     <div className="gallery-carousel">
                                         <div className="carousel-track">
                                             <div className="carousel-slide"><img src={`${basePath}images/general/grm_ottoman(1).webp`} alt="Ottoman Bed 1" /></div>
